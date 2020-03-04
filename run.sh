a2b3087bb96926dfbe978f46c0b1953588781ec2
Aya-ZIbra <56703597+Aya-ZIbra@users.noreply.github.com>
2020-03-04 14:52:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-cd MyYogini
 rm -rf output
 mkdir -p output/warrior1
 ./myYogini -m ./models/human-pose-estimation-0001/FP32/human-pose-estimation-0001.xml -i ./my_yogini/from_Chris/AI-Yogini-Project/badWarrior11.jpg -o  output/warrior1 -c ./my_yogini/from_Chris/AI-Yogini-Project/GoodWarrior1flipped.jpg -no_show -r -p warrior1 
